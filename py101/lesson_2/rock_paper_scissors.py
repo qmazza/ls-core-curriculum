2c8a9227bd7fa4413a6cdb579b98134778a6b500
Quinn Mazza <quinnmazza@gmail.com>
2024-06-25 13:56:01 -0700

    Walk-through RPS

--- py101/lesson_2/rock_paper_scissors.py
+++ py101/lesson_2/rock_paper_scissors.py
@@ -14,3 +14,67 @@ The user makes a choice.
 The computer makes a choice.
 The winner is displayed.
 """
+import random
+
+#CONSTANTS
+VALID_CHOICES = ['rock','paper','scissors']
+
+# Prompt message start helper funciton
+def prompt(message):
+    print(f'==> {message}')
+
+# Helper function to display winner
+def display_winner(player, computer):
+    if ((player == 'rock' and computer == 'scissors') or
+        (player == 'paper' and computer == 'rock') or
+        (player == 'scissors' and computer == 'paper')):
+        prompt('You win!')
+    elif ((player == 'rock' and computer == 'spaper') or
+        (player == 'paper' and computer == 'scissors') or
+        (player == 'scissors' and computer == 'rock')):
+        prompt('Computer wins!')
+    else:
+        prompt("It's a tie!")    
+
+
+
+while True:
+    #use .lower? . 
+    # Can interpolate valid_choce with f string
+    # ', ' with join to concatenate into a string.
+    prompt(f'Choose one: {', '.join(VALID_CHOICES)}')
+    choice = input()
+
+    # Validate user input
+    while choice not in VALID_CHOICES:
+        prompt("That's not a valid choice")
+        choice = input()
+
+    # Computers choice
+    computer_choice = random.choice(VALID_CHOICES)
+
+    # Display choices
+    prompt(f'You chose {choice}, computer chose {computer_choice}')
+
+    # function to check display the winner while being more readable and easier to change.
+    display_winner(choice, computer_choice)
+    
+    # Player Wins:
+    # rock beats scissors
+    # paper beats rock
+    # scissors beats paper
+
+    # choice = computer_choice tie
+
+    prompt('Do you want to play again? (y/n)')
+    answer = input().lower()
+    while True:
+        if answer.startswith('n') or answer.startswith('y'):# Checks for a valid input. Wont let user enter empty string.
+            break
+        
+        prompt('Please enter "y" or "n".')
+        answer = input().lower()
+
+    # After validating input that there is at least an index of 0, break loop.
+    if answer[0] == 'n':
+        break
