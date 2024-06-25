4e883dfa675eaa45ebe747d018cecc8e81ef6568
Quinn Mazza <quinnmazza@gmail.com>
2024-06-25 11:17:25 -0700

    New rock paper scissors assignment

---
+++ py101/lesson_2/rock_paper_scissors.py
@@ -0,0 +1,16 @@
+"""
+In this assignment, we'll build a Rock Paper Scissors game. 
+Rock Paper Scissors is a simple game played between two opponents. 
+Both opponents choose an item from rock, paper, or scissors. 
+The winner is decided according to the following rules:
+
+If player a chooses rock and player b chooses scissors, player a wins.
+If player a chooses paper and player b chooses rock, player a wins.
+If player a chooses scissors and player b chooses paper, player a wins.
+If both players choose the same item, neither player wins. It's a tie.
+Our version of the game lets the user play against the computer. The game flow should go like this:
+
+The user makes a choice.
+The computer makes a choice.
+The winner is displayed.
+"""
