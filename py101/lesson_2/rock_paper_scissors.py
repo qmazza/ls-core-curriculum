ce629971e9ab32252e7d01131bf05f89eef22b18
Quinn Mazza <quinnmazza@gmail.com>
2024-06-26 14:50:35 -0700

    Rock paper scissors updates

--- py101/lesson_2/rock_paper_scissors.py
+++ py101/lesson_2/rock_paper_scissors.py
@@ -5,7 +5,7 @@ import random
 import json
 import os
 
-# CONSTANTs
+# Constants and dictionaries
 VALID_CHOICES = ['rock','paper','scissors','lizard','spock',
 'r','p','s','l','sp']
 
@@ -17,7 +17,7 @@ DICT_CHOICES = {'r': 'rock',
                 'l': 'lizard',
                 'sp': 'spock'}
 
-winning_choice = {
+WINNING_CHOICE = {
     'rock': {'scissors', 'lizard'},
     'paper': {'rock', 'spock'},
     'scissors': {'paper', 'lizard'},
@@ -25,16 +25,35 @@ winning_choice = {
     'spock': {'scissors', 'rock'}
 }
 
+# Game Score
+player_wins = 0
+computer_wins = 0
+ties = 0
 
+
+# editing for winner of 5
 def display_winner(player, computer):
     '''This function checks for the winner'''
-    if computer in winning_choice[player]:
-        print(MESSAGES['player_wins'].center(40))
-    elif player in winning_choice[computer]:
-        print(MESSAGES['computer_wins'].center(40))
-    else:
-        print(MESSAGES['no_winner'].center(40))
+    while True:
+        if computer in WINNING_CHOICE[player]:
+            print(MESSAGES['player_wins'].center(40))
+            return 'player'
+        elif player in WINNING_CHOICE[computer]:
+            print(MESSAGES['computer_wins'].center(40))
+            return 'computer'
+        else:
+            print(MESSAGES['no_winner'].center(40))
+            return 'tie'
+        break
 
+def game_count(winner):
+   global player_wins, computer_wins, ties
+    if winner == 'player':
+        player_wins += 1
+    elif winner == 'computer':
+        computer_wins += 1
+    else:
+        ties += 1
 
 
 def message(key):
@@ -71,6 +90,26 @@ def display_choice():
     computer_choice=computer_choice).title())
 
 
+def score_check():
+    '''This function checks the score. If player or computer win
+    3 out of 5 times, the game is over and scores are reset'''
+    if player_wins == 3:
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print('Congratulations! You\'ve beat the computer and won the game!')
+        score_reset()
+    elif computer_wins == 3:
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print('Game over. Sorry, you\'ve lost to the computer.')
+        score_reset()
+
+def score_reset():
+    '''Resets the score if user wants to continue'''
+    global player_wins, computer_wins, ties
+    player_wins = 0
+    computer_wins = 0
+    ties = 0
+
+
 # Main loop to run the program
 while True:
     os.system('cls' if os.name == 'nt' else 'clear')
@@ -99,8 +138,20 @@ while True:
     os.system('cls' if os.name == 'nt' else 'clear')
     display_choice()
 
-    # Check results and display the winner
-    display_winner(user_choice, computer_choice)
+
+    # Check results and display the winner of round
+    result = display_winner(user_choice, computer_choice)
+
+    # Update score count
+    game_count(result)
+
+    # Display current scores
+    print(f"Player wins: {player_wins}")
+    print(f"Computer wins: {computer_wins}")
+    print(f"Ties: {ties}")
+
+    # Check for game winner.
+    score_check()
 
     # Ask the user to play again
     while True:
